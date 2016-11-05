4bdabd809659323a03f37553b888a097c968e0fd
Thomas GRANGE <tgrange@e1r11p22.42.fr>
2016-11-05 14:47:06 +0100

    Push auto

---
+++ Libft/Libft/rendu/ft_strlcat.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgrange <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/05 11:40:27 by tgrange           #+#    #+#             */
+/*   Updated: 2016/11/05 12:19:23 by tgrange          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size)
+{
+	return (ft_strlen(ft_strncat(dst, src, size)));
+}
---
+++ Libft/scripts/push.sh
@@ -0,0 +1,1 @@
+cp -r /Users/tgrange/42/* /Users/tgrange/perso/Libft/; cd /Users/tgrange/perso/Libft; git add .; git commit -m "Push auto"; git push
