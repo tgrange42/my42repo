c7b50f4ffcda6ca6ea4bf5a675d3b666e4d8683d
Thomas GRANGE <tgrange@e1r11p22.42.fr>
2016-11-05 14:48:38 +0100

    Push auto

---
+++ Libft/rendu/ft_strnstr.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgrange <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/04 16:49:50 by tgrange           #+#    #+#             */
+/*   Updated: 2016/11/04 18:24:15 by tgrange          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstrv(char *buf, const char *little, size_t len)
+{
+	size_t	beacon;
+	size_t	i;
+	size_t	j;
+
+	beacon = 0;
+	i = 0;
+	j = 0;
+	while (beacon < len + 1)
+	{
+		while ((i < len - 1 || j > 0 || (ft_strlen(little) == 1 && i < len)) \
+				&& buf[i] != '\0')
+		{
+			if (buf[i] == little[j])
+				j++;
+			else
+				j = 0;
+			if (little[j] == '\0')
+				return (&buf[i + 1 - ft_strlen(little)]);
+			i++;
+		}
+		beacon++;
+		i = beacon;
+	}
+	return (NULL);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	char	*res;
+
+	res = (char *)big;
+	if (ft_strcmp(little, "") != 0)
+		return (ft_strnstrv(res, little, len));
+	return (res);
+}
---
+++ scripts/push.sh
@@ -0,0 +1,1 @@
+cp -r /Users/tgrange/42/* /Users/tgrange/perso; cd /Users/tgrange/perso; git add .; git commit -m "Push auto"; git push
