26463a67d4ade280de5f8ed1e5a5d92a5e27e36a
Thomas GRANGE <tgrange@e1r11p22.42.fr>
2016-11-05 14:47:33 +0100

    Push auto

--- Libft/scripts/push.sh
+++ Libft/scripts/push.sh
@@ -1,1 +1,1 @@
-cp -r /Users/tgrange/42/* /Users/tgrange/perso/Libft/; cd /Users/tgrange/perso/Libft; git add .; git commit -m "Push auto"; git push
+cp -r /Users/tgrange/42/* /Users/tgrange/perso/Libft/; cd /Users/tgrange/perso; git add .; git commit -m "Push auto"; git push
