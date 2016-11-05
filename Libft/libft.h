89024e70a492242b7f24e66bc1da3bfa58a74e1d
Thomas GRANGE <tgrange@e1r11p22.42.fr>
2016-11-05 13:14:27 +0100

    First Push Libft

---
+++ Libft/libft.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgrange <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/05 10:09:13 by tgrange           #+#    #+#             */
+/*   Updated: 2016/11/05 12:18:47 by tgrange          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_digit(int c);
+int		ft_isalpha(int c);
+int		ft_atoi(const char *str);
+char	*ft_strstr(const char *big, const char *little);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size);
+#endif
