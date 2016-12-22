64777da9a2dcef7b6a95c3b4e6fa367783c7f291
Thomas GRANGE <tgrange@e1r8p18.42.fr>
2016-12-22 20:02:54 +0100

    laiduhaejpfemi0

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tgrange <tgrange@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/19 12:41:02 by tgrange           #+#    #+#             */
-/*   Updated: 2016/12/22 15:19:43 by tgrange          ###   ########.fr       */
+/*   Updated: 2016/12/22 17:54:57 by tgrange          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static t_gnl	*get_t_gnl(int fd, t_gnl **begin)
 	{
 		 if (!(tmp = (t_gnl *)ft_memalloc(sizeof(tmp))))
 		 	return (NULL);
-		 ft_strnew(tmp->buf);
+		 // ft_strnew(tmp->buf);
 		 tmp->next =NULL;
 		 tmp->fd = fd;
 		 tmp->i = 0;
@@ -34,9 +34,26 @@ static t_gnl	*get_t_gnl(int fd, t_gnl **begin)
 
 static int		get_the_line(t_gnl *this_fd, char **line)
 {
-	if (!this_fd->buf && read(this_fd->fd, this_fd->buf, BUFF_SIZE) == -1)
-		retur (-1);
-	while ()
+	int		n;
+	size_t	len;
+
+	n = 0;
+	*line = ft_strnew(0);
+	if (!this_fd->buf[this_fd->i] && read(this_fd->fd, this_fd->buf, BUFF_SIZE) == -1)
+		return (-1);
+	while (!n)
+	{
+		len = ft_strlento(this_fd->buf, (size_t)this_fd->i, '\n');
+		*line  = ft_strjoin(*line, ft_strsub(this_fd->buf, (unsigned int)this_fd->i, len));
+		this_fd->i += len;
+		if (!this_fd->buf[this_fd->i])
+		{
+			ft_strclr(this_fd->buf);
+			if (!read(this_fd->fd, this_fd->buf, BUFF_SIZE))
+				return (1);
+		}
+	}
+	return (0);
 }
 
 int				get_next_line(int fd, char **line)
